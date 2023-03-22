f8dbdcbcab3169094a5c8dd2b0408c5d75691398
dylanstewart06 <dylanstewart06@gmail.com>
2023-03-22 08:43:17 -0600

    Returing all routines when not logged in

--- src/apiAdapter/index.js
+++ src/apiAdapter/index.js
@@ -1,1 +1,13 @@
-const BASE_URL = "https://fitnesstracker-65db.onrender.com/api";
+const BASE_URL = "https://fitnesstracker-65db.onrender.com/api"
+export const getRoutines = async () => {
+    try {
+        const token = localStorage.getItem("token");
+        const response = await fetch(`${url}/routines`, {
+            headers:{
+                Authorization: `Bearer ${token}`,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
