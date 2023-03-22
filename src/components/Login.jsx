cabdd0111c69a25493807e4127a5bd0b89adb3e2
Logan <logancable@ymail.com>
2023-03-21 18:30:19 -0700

    made basic routes and components

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Login(props) {
+  return (
+    <>
+    <div>
+        <h1>Welcome to Login</h1>
+    </div>
+    </>
+  )
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,7 @@
 export { default as Main } from "./Main";
 export { default as Navbar } from "./Navbar";
+export { default as Login } from "./Login";
+export { default as HomePage } from "./HomePage";
+export { default as Registration } from "./Registration";
+export { default as Routines } from "./Routines";
+export { default as Activities } from "./Activities"; 
